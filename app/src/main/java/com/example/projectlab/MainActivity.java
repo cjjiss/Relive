059dd9eadc84bb0b51e880b9a206904895c96042
cjjiss <jisscj2@gmail.com>
2023-01-06 12:26:21 +0530

    commit before gradle

--- app/src/main/java/com/example/projectlab/MainActivity.java
+++ app/src/main/java/com/example/projectlab/MainActivity.java
@@ -1,31 +1,40 @@
 package com.example.projectlab;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
 //This is first page
 public class MainActivity extends AppCompatActivity {
-    Button b1;
+    ImageView v1,v2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b1 = (Button)findViewById(R.id.b1_user);
 
+        v1= findViewById(R.id.imageView);
+        v2 = findViewById(R.id.imageView3);
 
-        b1.setOnClickListener(new View.OnClickListener() {
+        v1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this,User_Login.class);
-                startActivity(i);
+
+                    Intent i = new Intent(MainActivity.this,User_Login.class);
+                    startActivity(i);
             }
         });
-
-
-
+        v2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i2 = new Intent(MainActivity.this,Hospital_login.class);
+                startActivity(i2);
+            }
+        });
     }
 }
